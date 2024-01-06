8623d5064f07f2dd72e9559795b9fc61e0fb0b7e
Carlos Pure Serrano <carlospures@gmail.com>
2024-01-06 18:42:11 -0500

    added front and back option to get commits

--- src/App.js
+++ src/App.js
@@ -3,28 +3,38 @@ import './App.css';
 
 function App() {
   const [commits, setCommits] = useState([]);
+  const [selectedOption, setSelectedOption] = useState(null);
+ 
 
-  useEffect(() => {
+  const filterCommits = (option) => {
 
-    fetch('http://localhost:3100/api/github')
+    fetch(`http://localhost:3100/api/github?name=${option}`)
       .then(response => response.json())
-      .then(data => setCommits(data))
-      .catch(error => console.error('Error fetching commits:', error));
-  }, []);
+      .then(data => {
+        setCommits(data);
+        setSelectedOption(option);
+      })
+      .catch(error => console.error(`Error fetching ${option} commits:`, error));
+  };
+ 
 
   return (
     <div className="App">
-      <h1>Lista de Commits de GitHub</h1>
+      <h1>GitHub Commits</h1>
+      <div>
+        <button onClick={() => filterCommits('front')}>Commits Frontend</button>
+        <button onClick={() => filterCommits('FullTimeForce')}>Commits Backend</button>
+      </div>
       <ul>
         {commits.map(commit => (
           <li key={commit.sha}>
             {commit.commit.author.name} <span> : </span>
-          <strong>
-            <a href={commit.html_url} target="_blank" rel="noopener noreferrer">
-              {commit.commit.message}
-            </a>
-          </strong>
-        </li>
+            <strong>
+              <a href={commit.html_url} target="_blank" rel="noopener noreferrer">
+                {commit.commit.message}
+              </a>
+            </strong>
+          </li>
         ))}
       </ul>
     </div>
