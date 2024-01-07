5c56c7125b95d0d53be11147cd0461434fc7fa3b
Carlos Pure Serrano <carlospures@gmail.com>
2024-01-06 19:03:16 -0500

    added some styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   const [commits, setCommits] = useState([]);
   const [selectedOption, setSelectedOption] = useState(null);
- 
 
   const filterCommits = (option) => {
-
     fetch(`http://localhost:3100/api/github?name=${option}`)
       .then(response => response.json())
       .then(data => {
@@ -16,27 +14,40 @@ function App() {
       })
       .catch(error => console.error(`Error fetching ${option} commits:`, error));
   };
- 
 
   return (
     <div className="App">
       <h1>GitHub Commits</h1>
-      <div>
-        <button onClick={() => filterCommits('front')}>Commits Frontend</button>
-        <button onClick={() => filterCommits('FullTimeForce')}>Commits Backend</button>
+      <div className="center-container">
+        <div className="button-container">
+          <button onClick={() => filterCommits('front')}>Frontend</button>
+          <button onClick={() => filterCommits('FullTimeForce')}>Backend</button>
+        </div>
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th>Author</th>
+                <th>Commit Message</th>
+              </tr>
+            </thead>
+            <tbody>
+              {commits.map(commit => (
+                <tr key={commit.sha}>
+                  <td>{commit.commit.author.name}</td>
+                  <td>
+                    <strong>
+                      <a href={commit.html_url} target="_blank" rel="noopener noreferrer">
+                        {commit.commit.message}
+                      </a>
+                    </strong>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-      <ul>
-        {commits.map(commit => (
-          <li key={commit.sha}>
-            {commit.commit.author.name} <span> : </span>
-            <strong>
-              <a href={commit.html_url} target="_blank" rel="noopener noreferrer">
-                {commit.commit.message}
-              </a>
-            </strong>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
